3badef174b784d576bd418fe575bea6a38252452
HenriqueCezana <cezanahenrique@gmail.com>
2025-12-05 13:49:30 -0300

    Criei as funções de cadastrar, editar e excluir competição no control de competições

---
+++ control/competicaoControl.js
@@ -0,0 +1,135 @@
+import { Competicao } from "../Competicao.js";
+import { Maratona } from "../Maratona.js";
+import { CompeticaoTrilha } from "../CompeticaoTrilha.js";
+
+var listaCompeticoes = [];
+
+function gerarID() {
+    if (listaCompeticoes.length == 0) {
+        return 0;
+    } else {
+        let maiorID = listaCompeticoes[0].idCompeticao;
+
+        for (let i = 1; i < listaCompeticoes.length; i++) {
+            if (listaCompeticoes[i].idCompeticao > maiorID) {
+                maiorID = listaCompeticoes[i].idCompeticao;
+            }
+        }
+
+        return maiorID + 1;
+    }
+}
+
+export function cadastrarCompeticao(obj) {
+
+    let novoID = gerarID();
+    let novaCompeticao;
+
+    if (obj.tipo == "TRILHA") {
+        novaCompeticao = new CompeticaoTrilha(
+            novoID,
+            obj.nome,
+            obj.data,
+            obj.local,
+            obj.distancia,
+            obj.limiteParticipante,
+            obj.preco,
+            obj.limiteTempoMinutos,
+            obj.qtdCheckPoint,
+            obj.grauDificuldade
+        );
+    } else if (obj.tipo == "MARATONA") {
+        novaCompeticao = new Maratona(
+            novoID,
+            obj.nome,
+            obj.data,
+            obj.local,
+            obj.distancia,
+            obj.limiteParticipante,
+            obj.preco,
+            obj.limiteTempoMinutos,
+            obj.altimetria
+        );
+    }
+
+    listaCompeticoes.push(novaCompeticao);
+    return listaCompeticoes;
+}
+
+
+export function editarCompeticao(id, dados) {
+
+    let competicao = null;
+
+    for (let i = 0; i < listaCompeticoes.length; i++) {
+        if (listaCompeticoes[i].idCompeticao == id) {
+            competicao = listaCompeticoes[i];
+            break;
+        }
+    }
+
+    if (competicao == null) {
+        return "Competição não encontrada!";
+    }
+
+
+    if (dados.nome !== undefined) {
+        competicao.nome = dados.nome;
+    }
+
+    if (dados.data !== undefined) {
+        competicao.data = dados.data;
+    }
+
+    if (dados.local !== undefined) {
+        competicao.local = dados.local;
+    }
+
+    if (dados.distancia !== undefined) {
+        competicao.distancia = dados.distancia;
+    }
+
+    if (dados.limiteParticipante !== undefined) {
+        competicao.limiteParticipante = dados.limiteParticipante;
+    }
+
+    if (dados.preco !== undefined) {
+        competicao.preco = dados.preco;
+    }
+
+    if (dados.limiteTempoMinutos !== undefined) {
+        competicao.limiteTempoMinutos = dados.limiteTempoMinutos;
+    }
+
+
+    if (competicao.qtdCheckPoint !== undefined && dados.qtdCheckPoint !== undefined) {
+        competicao.qtdCheckPoint = dados.qtdCheckPoint;
+    }
+
+    if (competicao.grauDificuldade !== undefined && dados.grauDificuldade !== undefined) {
+        competicao.grauDificuldade = dados.grauDificuldade;
+    }
+
+    if (competicao.altimetria !== undefined && dados.altimetria !== undefined) {
+        competicao.altimetria = dados.altimetria;
+    }
+
+    return competicao;
+
+}
+
+
+export function excluirCompeticao(id){
+
+    for (let i = 0; i < listaCompeticoes.length; i++) {
+        if (listaCompeticoes[i].idCompeticao == id) {
+            let removida = listaCompeticoes[i];
+            listaCompeticoes.splice(i, 1);
+            return removida;
+        }
+    }
+
+    return null;
+
+}
+
