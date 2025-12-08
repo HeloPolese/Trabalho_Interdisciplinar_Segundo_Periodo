9edadf58c64bcf8f627673117a3b4963ee764765
Heloisa <poleseheloisa24@gmail.com>
2025-12-08 08:06:46 -0300

    correções na classe competidor

--- Competidor.js
+++ Competidor.js
@@ -2,64 +2,56 @@ import Atleta from "./Atleta.js";
 import Competicao from "./Competicao.js";
 
 export class Competidor extends Atleta {
-
+    #posicao;
     #refCompeticao;
     #refAtleta;
     #tempoMinutos;
 
-    constructor(refCompeticao, refAtleta, tempoMinutos = null, nome, idade = "", cpf, posicao) {
+    constructor(nome, idade = "", cpf, refCompeticao, refAtleta, tempoMinutos = null, posicao) {
         super(nome, idade, cpf);
         this.posicao = posicao;
-        this.#refCompeticao = refCompeticao;
+        this.refCompeticao = refCompeticao;
+        this.refAtleta = refAtleta;
         this.#tempoMinutos = tempoMinutos;
     }
 
-    get nome() {
-        return super.nome;
-    }
-    get idade() {
-        return super.idade;
-    }
-    get cpf() {
-        return super.cpf;
-    }
-    get posicao() { 
-        return this.posicao;
+    get posicao() {
+        return this.#posicao;
     }
     get refCompeticao() {
         return this.#refCompeticao;
-    }   
+    }
     get refAtleta() {
         return this.#refAtleta;
     }
     get tempoMinutos() {
         return this.#tempoMinutos;
-    }   
+    }
     set tempoMinutos(novoTempoMinutos) {
         if (novoTempoMinutos != undefined && novoTempoMinutos >= 0) {
             this.#tempoMinutos = novoTempoMinutos;
         }
-    }   
-    set refCompeticao(competicao){
-        if(competicao != undefined && competicao instanceof Competicao){
-            this.#refCompeticao = competicao.idCompeticao;
+    }
+    set refCompeticao(competicao) {
+        if (competicao != undefined && competicao instanceof Competicao) {
+            this.#refCompeticao = competicao;
         }
     }
 
-    set refAtleta(atleta){
-        if(atleta != undefined && atleta instanceof Atleta){
-            this.#refAtleta = atleta.idAtleta;
-        }   
-    }  
-    set posicao(novaPosicao){
-        if(novaPosicao != undefined && novaPosicao > 0 && novaPosicao < Competicao.limiteParticipante){
-            this.posicao = novaPosicao;
-        }   
+    set refAtleta(atleta) {
+        if (atleta != undefined && atleta instanceof Atleta) {
+            this.#refAtleta = atleta;
+        }
+    }
+    set posicao(novaPosicao) {
+        if (novaPosicao != undefined && novaPosicao > 0 && novaPosicao <= Competicao.limiteParticipante) {
+            this.#posicao = novaPosicao;
+        }
     }
 
-    toString(){
-        return  super.toString() +
-                `\nPosição: ${this.posicao}` +
-                `\nTempo em minutos: ${this.tempoMinutos}`;
+    toString() {
+        return super.toString() +
+            `\nPosição: ${this.posicao}` +
+            `\nTempo em minutos: ${this.tempoMinutos}`;
     }
 }
